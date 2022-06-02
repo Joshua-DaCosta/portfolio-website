379038fa5b079caaae335d11fd7d193f4a554a50
Joshua Da Costa <Joshua.0331@hotmail.com>
2022-06-02 19:37:40 -0400

    add itype anf arrow animation to intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,24 @@
 import "./intro.scss";
 import baki from '../../assets/baki.png';
 import { KeyboardArrowDown } from "@material-ui/icons";
-
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 
 const Intro = () => {
+
+  const textRef = useRef();
+  console.log(textRef);
+
+  useEffect(() => {
+   init(textRef.current, {
+     showCursor: true,
+     backDelay: 1500,
+     backSpeed: 70,
+     strings: ["Developer", "Designer", "Engineer"]
+   });
+  }, []);
+
   return (
     <div id="intro" className="intro">
       <div className="left">
@@ -16,10 +30,10 @@ const Intro = () => {
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
           <h1>Joshua Da Costa</h1>
-          <h3>Developer</h3>
+          <h3>Software <span ref={textRef}></span></h3>
         </div>
         <a href="#portfolio">
-          <KeyboardArrowDown/>
+          <KeyboardArrowDown className="arrow" />
         </a>
       </div>
     </div>
