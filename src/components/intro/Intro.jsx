954902a40ca328d17bb6022020afafb9cd5342f7
Joshua Da Costa <Joshua.0331@hotmail.com>
2022-06-01 20:09:01 -0400

    finish right side of intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,31 @@
 import "./intro.scss";
+import baki from '../../assets/baki.png';
+import { KeyboardArrowDown } from "@material-ui/icons";
+
+
 
 const Intro = () => {
-  return <div id="intro" className="intro"></div>;
+  return (
+    <div id="intro" className="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src={baki} alt="baki" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Joshua Da Costa</h1>
+          <h3>Developer</h3>
+        </div>
+        <a href="#portfolio">
+          <KeyboardArrowDown/>
+        </a>
+      </div>
+    </div>
+  );
+  
+ 
 }
 
 export default Intro
