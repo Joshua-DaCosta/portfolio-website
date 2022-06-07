717d130fea4f166398a3281cb9afe9758df266a4
Joshua Da Costa <Joshua.0331@hotmail.com>
2022-06-07 19:27:30 -0400

    finish up contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,36 @@
+import React, { useState } from "react";
 import "./contact.scss";
 
 const Contact = () => {
+  const [message, setMessage] = useState(false);
+
+   const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessage(true);
+    await setTimeout(() => {
+      setMessage(false);
+    }, 5000);
+  }
+
   return (
-    <section id="contact" className="contact"></section>
-  )
+    <section id="contact" className="contact">
+      <div className="left">
+        <img
+          src="https://www.htcinc.net/wp-content/uploads/2020/07/shake-hands-yellow-1.png"
+          alt="shake hands"
+        />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit" prev>Send</button>
+          {message && <span>Thank you, I will reach out ASAP :)</span>}
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact
