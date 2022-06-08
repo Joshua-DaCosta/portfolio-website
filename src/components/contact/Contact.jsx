b8f4780c3904ae0a5cda543fe23888eec5f47aa3
Joshua Da Costa <Joshua.0331@hotmail.com>
2022-06-07 22:06:43 -0400

    made contacts responsive

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,17 +4,21 @@ import "./contact.scss";
 const Contact = () => {
   const [message, setMessage] = useState(false);
 
-   const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setMessage(true);
     await setTimeout(() => {
       setMessage(false);
     }, 5000);
-  }
+  };
 
   return (
     <section id="contact" className="contact">
       <div className="left">
+        {message && (
+          <p className="mobile">Thank you, I will reach out ASAP &#128516;</p>
+        )}
+
         <img
           src="https://www.htcinc.net/wp-content/uploads/2020/07/shake-hands-yellow-1.png"
           alt="shake hands"
@@ -28,11 +32,15 @@ const Contact = () => {
           <button type="submit" prev>
             Send
           </button>
-          {message && <p>Thank you, I will reach out ASAP &#128516;</p>}
+          {message && (
+            <p className="desktop">
+              Thank you, I will reach out ASAP &#128516;
+            </p>
+          )}
         </form>
       </div>
     </section>
   );
-}
+};
 
-export default Contact
+export default Contact;
